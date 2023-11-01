ad77d957d8ad15a2e90b9aa81b1b63c70f298654
gopalbasak1 <gopalbasak1223@gmail.com>
2023-11-01 12:08:34 +0600

    create dragon news file and completed header navbar and home section

---
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -0,0 +1,52 @@
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import qZone1 from '../../../assets/qZone1.png';
+import qZone2 from '../../../assets/qZone2.png';
+import qZone3 from '../../../assets/qZone3.png';
+
+
+const RightSideNav = () => {
+    return (
+        <div>
+
+           <div className='p-4 space-y-3 mb-6'>
+            <h2 className="text-3xl font-bold">Login With</h2>
+        <button className="btn btn-outline w-full">
+        <FaGoogle></FaGoogle>
+        Google
+        </button>
+        <button className="btn btn-outline w-full">
+        <FaGithub></FaGithub>
+        Github
+        </button>
+           </div>
+
+           <div className='p-4 mb-6'>
+            <h2 className="text-3xl mb-4 font-bold">Find Us On</h2>
+            <a className="p-4 flex text-lg items-center border rounded-t-lg">
+                <FaFacebook className='mr-3'></FaFacebook>
+                <span>Facebook</span>
+            </a>
+            <a className="p-4 flex text-lg items-center border-x ">
+                <FaTwitter className='mr-3'></FaTwitter>
+                <span>Twitter</span>
+            </a>
+            <a className="p-4 flex text-lg items-center border rounded-b-lg">
+               <FaInstagram className='mr-3'></FaInstagram>
+               <span>Instagram</span>
+            </a>
+           </div>
+
+        {/*Q-Zone*/}
+        <div className='p-4 space-y-3 mb-6 bg-[#f3f3f3]'>
+            <h2 className="text-3xl mb-4 font-bold">Q Zone</h2>
+            <img src={qZone1}alt="" />
+            <img src={qZone2}alt="" />
+            <img src={qZone3}alt="" />
+   
+           </div>
+
+        </div>
+    );
+};
+
+export default RightSideNav;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXPr1cdc7D4avObRtIarK5tXitHF9SdAU",
+  authDomain: "react-dragon-news-804a4.firebaseapp.com",
+  projectId: "react-dragon-news-804a4",
+  storageBucket: "react-dragon-news-804a4.appspot.com",
+  messagingSenderId: "452351225260",
+  appId: "1:452351225260:web:b8c0ab89074a4b13d09e09"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
